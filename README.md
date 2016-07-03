2475b58b02e366a5bf47d83082cb8c1e4cf3b48f
Bucky <buckyroberts@gmail.com>
2016-07-03 02:09:33 -0400

    Added more docs

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Boilerplate for a React / Redux build.
 
 ![](http://i.imgur.com/2PAD0kp.png)
+
+## Containers vs. Components
+
+Containers are very similar to components, the only difference is that components are aware of application state. If
+part of your webpage is only used for displaying data (dumb) then make it a component. If you need it to be smart and
+aware of the state (whenever data changes) in the application then make it a container.
--- dev/js/reducers/users.js
+++ dev/js/reducers/users.js
@@ -1,39 +1,23 @@
 /*
- * Reducers take in the current state of app, action being performed, and return new state (new object)
- *      - must always return something (even empty state object if needed)
+ * The users reducer will always return an array of users no matter what
+ * You need to return something, so if there are no users then just return an empty array
  * */
 
-// Individual user
-const user = (state = {}, action) => {
-    switch (action.type) {
-        case 'ADD_USER':
-            return {
-                id: action.id,
-                first: action.first,
-                last: action.last,
-                age: action.age,
-                description: action.description
-            };
-        default:
-            return state
-    }
-};
-
-// All users
-const users = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_USER':
-            return [
-                ...state,
-                user(undefined, action)
-            ];
-        case 'TOGGLE_TODO':
-            return state.map(t =>
-                user(t, action)
-            );
-        default:
-            return state
-    }
-};
-
-export default users
+export default function () {
+    return [
+        {
+            id: 1,
+            first: "Bucky",
+            last: "Roberts",
+            age: 71,
+            description: "Bucky is a React developer and YouTuber"
+        },
+        {
+            id: 2,
+            first: "Joby",
+            last: "Wasilenko",
+            age: 27,
+            description: "Joby loves the Packers, cheese, and turtles."
+        }
+    ]
+}
