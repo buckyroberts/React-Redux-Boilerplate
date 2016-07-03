4fe79ae5ee3c03d45750cdd12bf9c215116a2218
Bucky <buckyroberts@gmail.com>
2016-07-03 03:36:53 -0400

    Everything works.

--- dev/js/containers/user-detail.js
+++ dev/js/containers/user-detail.js
@@ -6,23 +6,19 @@ import {connect} from 'react-redux';
  * */
 
 class UserDetail extends Component {
-
     render() {
-
         if (!this.props.user) {
             return (<div>Select a user...</div>);
         }
-
         return (
             <div>
                 <img src={this.props.user.thumbnail} />
-                <h3>{this.props.user.first} {this.props.user.last}</h3>
-                <div>Age: {this.props.user.age}</div>
-                <div>Description: {this.props.user.description}</div>
+                <h2>{this.props.user.first} {this.props.user.last}</h2>
+                <h3>Age: {this.props.user.age}</h3>
+                <h3>Description: {this.props.user.description}</h3>
             </div>
         );
     }
-
 }
 
 // "state.activeUser" is set in reducers/index.js
--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -19,7 +19,15 @@ export default function () {
             last: "Wasilenko",
             age: 27,
             description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/rVASmUK.jpg"
+            thumbnail: "http://i.imgur.com/52xRlm8.png"
+        },
+        {
+            id: 3,
+            first: "Madison",
+            last: "Williams",
+            age: 24,
+            description: "Madi likes her dog but it is really annoying.",
+            thumbnail: "http://i.imgur.com/4EMtxHB.png"
         }
     ]
 }
