768a123b113977606a5f412d8771232e03d68ba1
sabrym <sabry.moulana@gmail.com>
2016-11-06 23:17:41 +0530

    Removed unnecessary break statement. Will never be reached because of the return

--- dev/js/reducers/reducer-active-user.js
+++ dev/js/reducers/reducer-active-user.js
@@ -8,7 +8,7 @@ export default function (state = null, action) {
     switch (action.type) {
         case 'USER_SELECTED':
             return action.payload;
-            break;
     }
+    
     return state;
 }
