29a4bac5c6e3b065b4fe7f8dd16b763cc9c494df
Luqman <luqmanrom@gmail.com>
2017-10-12 12:12:41 +0800

    Add stylings for burger button

--- dev/js/index.js
+++ dev/js/index.js
@@ -25,13 +25,48 @@ const store = createStore(
 
 const Wrap = ({children}) => children;
 
+var styles = {
+  bmBurgerButton: {
+    position: 'fixed',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenu: {
+    background: '#373a47',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em'
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
+
 
 ReactDOM.render(
     <Provider store={store}>
     	<div id="outerContainer">
     		<Header/>
 
-			<Menu pageWrapId={ "pageWrap" } outerContainerId={ "outerContainer" }>
+			<Menu styles={ styles } pageWrapId={ "pageWrap" } outerContainerId={ "outerContainer" }  width={ '20%' }>
 				<a id="home" className="menu-item" href="/">Home</a>
 				<a id="about" className="menu-item" href="/about">About</a>
 				<a id="contact" className="menu-item" href="/contact">Contact</a>
